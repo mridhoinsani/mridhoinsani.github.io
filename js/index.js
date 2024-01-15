429e17ced941f668a96b5dd0572fac2e477fd507
mridhoinsani <studyridho@gmail.com>
2024-01-15 21:13:44 +0700

    first commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 function hitungLuas() {
-    let inputAlas = document.getElementById ("input-alas").value;
-    let inputTinggi = document.getElementById ("input-tinggi").value;
-    let hasilLuas = (parseInt(inputAlas) * parseInt (inputTinggi))/2;
+    let inputSisiL = document.getElementById ("input-sisi-luas").value;
+    let hasilLuas = parseInt(inputSisiL) * parseInt (inputSisiL);
     document.getElementById("keluaranLuas").innerHTML=hasilLuas;
 }
 
 function hitungKeliling(){
-    let inputSisi1 = document.getElementById ("input-sisi1").value
-    let inputSisi2 = document.getElementById ("input-sisi2").value
-    let inputSisi3 = document.getElementById ("input-sisi3").value
-    let hasilKeliling = parseInt(inputSisi1) + parseInt(inputSisi2) + parseInt(inputSisi3)
+    let inputSisiK = document.getElementById ("input-sisi-keliling").value
+    let hasilKeliling = parseInt(inputSisiK) * 4;
     document.getElementById("keluaranKeliling").innerHTML=hasilKeliling;
 }
 
