99714dcfd752760e3916940932333e6ba3e8bee5
mridhoinsani <studyridho@gmail.com>
2024-02-18 12:50:42 +0700

    segitiga katik

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 function hitungLuas() {
-    let inputSisiL = document.getElementById ("input-sisi-luas").value;
-    let hasilLuas = parseInt(inputSisiL) * parseInt (inputSisiL);
+    let inputAlas = document.getElementById ("input-alas").value;
+    let inputTinggi = document.getElementById ("input-tinggi").value;
+    let hasilLuas = (parseInt(inputAlas) * parseInt (inputTinggi))/2;
     document.getElementById("keluaranLuas").innerHTML=hasilLuas;
 }
 
 function hitungKeliling(){
-    let inputSisiK = document.getElementById ("input-sisi-keliling").value
-    let hasilKeliling = parseInt(inputSisiK) * 4;
+    let inputSisi1 = document.getElementById ("input-sisi1").value
+    let inputSisi2 = document.getElementById ("input-sisi2").value
+    let inputSisi3 = document.getElementById ("input-sisi3").value
+    let hasilKeliling = parseInt(inputSisi1) + parseInt(inputSisi2) + parseInt(inputSisi3)
     document.getElementById("keluaranKeliling").innerHTML=hasilKeliling;
 }
 
